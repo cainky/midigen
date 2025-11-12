3614b910c2937d4182e02ff19dc69dfac1c16cfc
Kyle Cain <kylecain.me@gmail.com>
2025-11-12 14:50:17 -0500

    feat: export ArpeggioPattern and Scale in public API
    Added ArpeggioPattern enum and Scale class to the public API exports
    to make these commonly used classes more accessible to library users.

--- midigen/__init__.py
+++ midigen/__init__.py
@@ -1,8 +1,9 @@
 from .midigen import MidiGen
 from .track import Track
 from .note import Note
-from .chord import Chord, ChordProgression, Arpeggio
+from .chord import Chord, ChordProgression, Arpeggio, ArpeggioPattern
 from .key import Key, KEY_MAP
+from .scale import Scale
 from .drums import DrumKit
 from .song import Song
 from .section import Section
